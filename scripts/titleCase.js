018bb732873999815628526421187669471c3156
Jordan Henderson <hendersonj219@gmail.com>
2017-04-14 15:21:48 -0700

    Complete titleCase

--- scripts/titleCase.js
+++ scripts/titleCase.js
@@ -1,4 +1,8 @@
 function titleCase(str){
-	return str.split(' ').map(e => e.split('')).replace();
+	return str.split(' ')
+						.map((e) => e.split('')
+						.map((e,i) => i === 0 ? e.toUpperCase() : e ))
+						.map((e,i) => e.join(""))
+						.join(" ");
 }
 console.log(titleCase("I'm a little tea pot"));
