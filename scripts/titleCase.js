44f146f4e179daf27f74e59f7cd225332fd7725c
Jordan Henderson <hendersonj219@gmail.com>
2017-04-14 15:36:26 -0700

    Fix failed test case

--- scripts/titleCase.js
+++ scripts/titleCase.js
@@ -1,8 +1,8 @@
 function titleCase(str){
-	return str.split(' ')
+	return str.toLowerCase().split(' ')
 						.map((e) => e.split('')
 						.map((e,i) => i === 0 ? e.toUpperCase() : e ))
 						.map((e,i) => e.join(""))
 						.join(" ");
 }
-console.log(titleCase("I'm a little tea pot"));
+console.log(titleCase("sHoRt AnD sToUt"));
