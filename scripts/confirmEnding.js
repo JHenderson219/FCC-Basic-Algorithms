c4aa9ffb85e22eadcefea785c88347e105f15c96
Jordan Henderson <hendersonj219@gmail.com>
2017-04-26 15:04:02 -0700

    Complete confirmEnding.js

--- scripts/confirmEnding.js
+++ scripts/confirmEnding.js
@@ -1,6 +1,5 @@
 function confirmEnding(str,target){
-	const TGT_LEN = target.length;
-	return target ? str.slice(str.length-TGT_LEN,str.length) === target : undefined;
+	return target ? str.slice(str.length-target.length,str.length) === target : undefined;
 }
 
 console.log(confirmEnding("Bastian", "ian"));
